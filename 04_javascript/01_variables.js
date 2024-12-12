84969a9dc225dc603649986482ecb346bd3a9532
shyam kumar <shyam85.6kumar@gmail.com>
2024-12-12 12:03:09 +0000

    container configue don

---
+++ 04_javascript/01_variables.js
@@ -0,0 +1,13 @@
+const AccountId=1234;
+let Accountmail="shyam@gmail.com";
+var Accountpassword="9953";
+Accountcity="jaiput";
+
+
+// AccountId=2;
+Accountmail="ram@gmail.com";
+Accountpassword="1253";
+Accountcity="bangaluru";
+
+console.log(Accountcity)
+console.table([AccountId,Accountmail,Accountpassword,Accountcity])
