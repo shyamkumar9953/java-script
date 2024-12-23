d7eacf805de3ecb53d7711891a95371371525703
shyam kumar <shyam85.6kumar@gmail.com>
2024-12-23 20:07:46 +0000

    convertion of datatype

---
+++ 04_javascript/data_type.js
@@ -0,0 +1,24 @@
+let rollno = 171;
+let afterconvert = String(rollno);
+//console.log(typeof(rollno));
+//console.log(rollno);
+//console.log(typeof(afterconvert));
+
+let stringno = "171";
+//let converttono=Number(stringno);
+
+//console.log(typeof(converttono));
+
+let bool = 1;
+//console.log(bool);
+
+let no = "33abc";
+let convertinboolean = Boolean(no);
+let convertintonumber=Number(no);
+console.log(convertinboolean);
+console.log(typeof convertinboolean)
+console.log(convertintonumber);
+
+
+// this is data  type convertion 
+
