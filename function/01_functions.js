9c780e1ff414ecfb82db20e48e9a75bf3b710c8c
shyam kumar <shyam85.6kumar@gmail.com>
2025-01-06 15:46:50 +0000

    functions

---
+++ function/01_functions.js
@@ -0,0 +1,10 @@
+// node function/01_functions.js
+function SayMyName(){
+    console.log("shyam");
+}
+// SayMyName()
+function addtwono(n1,n2){
+    return n1+n2;
+}
+
+console.log("result:",addtwono(20,20));
