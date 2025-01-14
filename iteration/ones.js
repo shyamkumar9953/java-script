b8970ecb1a55a00001bfd1f7192fcccb7246850e
shyam kumar <shyam85.6kumar@gmail.com>
2025-01-14 15:05:12 +0000

    iteratuions

---
+++ iteration/ones.js
@@ -0,0 +1,16 @@
+// node iteration/ones.js
+
+// for(let i=0;i<10;i++){
+//     console.log(i);  
+// }
+
+// for(let i=0;i<10;i++){
+//     for(let j=0;j<10;j++){
+//         if(j==5){
+//             break;
+//         }
+//         console.log(j);
+        
+//     }
+//  }
+    
