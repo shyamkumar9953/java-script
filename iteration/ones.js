501440dfeca740aafb9fff0e6163e66b21732bb7
shyam kumar <shyam85.6kumar@gmail.com>
2025-01-15 14:39:01 +0000

    don

--- iteration/ones.js
+++ iteration/ones.js
@@ -14,3 +14,23 @@
 //     }
 //  }
     
+// const coding=["c++","js","py","swift","ru"];
+//  coding.forEach((item)=>{
+//     console.log(item);
+    
+//  })
+
+const nums=[1,2,3,4,5,6,7,8,9,10];
+// const mynewnums=nums.filter((num)=>{
+//     return num>2;
+// });
+// console.log(mynewnums);
+
+const arr=[];
+nums.forEach((num)=>{
+    if(num>2){
+        arr.push(num);
+    }
+})
+console.log(arr);
+
